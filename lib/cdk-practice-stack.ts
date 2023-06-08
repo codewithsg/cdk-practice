59859fb6b7dc380312d8848ab0713031202f4037
Saragam Ghimire <codewithghimire@gmail.com>
2023-06-08 13:00:07 +0545

    initial commit

--- lib/cdk-practice-stack.ts
+++ lib/cdk-practice-stack.ts
@@ -5,15 +5,22 @@ import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 
 export class CdkPracticeStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkPracticeQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+constructor(scope: Construct, id: string, props ?: StackProps) {
+  super(scope, id, {
+    ...props,
+    env:{
+      account: process.env.AWS_ACCESS_KEY_ID,
+      region: process.env.AWS_REGION
+    }
+  });
 
-    const topic = new sns.Topic(this, 'CdkPracticeTopic');
+  const queue = new sqs.Queue(this, 'CdkPracticeQueue', {
+    visibilityTimeout: Duration.seconds(300)
+  });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+  const topic = new sns.Topic(this, 'CdkPracticeTopic');
+
+  topic.addSubscription(new subs.SqsSubscription(queue));
+}
 }
