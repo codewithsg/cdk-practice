b38c11b9de7149706d13fa9576c307ac8559a9fe
Saragam Ghimire <codewithghimire@gmail.com>
2023-06-09 11:40:38 +0545

    ec2 instance

--- lib/cdk-practice-stack.ts
+++ lib/cdk-practice-stack.ts
@@ -1,26 +1,49 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { App, CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class CdkPracticeStack extends Stack {
 
-constructor(scope: Construct, id: string, props ?: StackProps) {
-  super(scope, id, {
-    ...props,
-    env:{
-      account: process.env.AWS_ACCESS_KEY_ID,
-      region: process.env.AWS_REGION
-    }
-  });
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, {
+      ...props,
+      env: {
+        account: process.env.AWS_ACCESS_KEY_ID,
+        region: process.env.AWS_REGION
+      }
+    });
 
-  const queue = new sqs.Queue(this, 'CdkPracticeQueue', {
-    visibilityTimeout: Duration.seconds(300)
-  });
+    /* Defining VPC */
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: 'PublicSubnet',
+        }]
+    });
 
-  const topic = new sns.Topic(this, 'CdkPracticeTopic');
+    /* Defining security groups */
+    // const securityGroup = new ec2.SecurityGroup(this, 'PracticeInstanceSecurityGroup', {
+    //   vpc,
+    //   allowAllOutbound: true /* allow all outbound traffic */
+    // })
+    // /* Setting inboud rules */
+    // securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH access');
+    // securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP access')
 
-  topic.addSubscription(new subs.SqsSubscription(queue));
-}
+    /* Defining EC2 instance */
+    const instance = new ec2.Instance(this, 'PracticeInstance', {
+      vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: ec2.MachineImage.genericLinux({
+        'ap-south-1': 'ami-000001'
+      })
+    })
+
+    /* Ouput for public IP address of the instance */
+    new CfnOutput(this, 'EC2InstancePublicId', {
+      value: instance.instancePublicIp
+    });
+  }
 }
